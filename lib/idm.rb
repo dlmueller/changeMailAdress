47c09af504732fda750390cd6c496ae333614f7b
Michael Schaarschmidt <michael@schaaryworks.com>
2015-07-17 17:25:00 +0200

    add some functions to idm
    add rename exist? and exist_uid to mail_exist? and uid_exist? and
    made the oposite functions as well as chnage function to change the
    mail address of a given uid and verify? to check, whether the
    address has been changed.

--- lib/idm.rb
+++ lib/idm.rb
@@ -7,9 +7,31 @@ class Idm
     connect
   end
 
-  def exist?(mail:)
+  def mail_exist?(mail:)
     result = plsql.mail_pkg.exist mail
     result == 1 ? true : false
+  end
+
+  def mail_not_exist?(mail:)
+    !mail_exist?(mail: mail)
+  end
+
+  def uid_exist?(uid:)
+    plsql.account_pkg.exist(uid)
+  end
+
+  def uid_not_exist?(uid:)
+    !uid_exist?(uid: uid)
+  end
+
+  def change(uid:,mail:)
+    plsql.mail_pkg.updateMailAddress(uid, mail)
+  end
+
+  def verify?(uid:,mail:)
+    current_mail = plsql.mail_pkg.getMailAddress(uid)
+
+    current_mail == mail ? true : false
   end
 
 private
